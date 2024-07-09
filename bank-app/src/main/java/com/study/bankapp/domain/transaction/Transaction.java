974e827e8e1dd36204ed937a714e98f0695e54f4
YouchanKim <dbcks4646@gmail.com>
2024-07-09 23:57:53 +0900

    Transaction Entity 생성 - 거래내역 Domain

---
+++ bank-app/src/main/java/com/study/bankapp/domain/transaction/Transaction.java
@@ -0,0 +1,78 @@
+package com.study.bankapp.domain.transaction;
+
+import com.study.bankapp.domain.account.Account;
+import com.study.bankapp.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor // 스프링이 USER 객체 생성할 때 빈 생성자로 new를 하기 때문에 필요함
+@EntityListeners(AuditingEntityListener.class)  // 자동으로 시간 기록할려면 이거 필요함
+@Getter
+@Table(name = "transaction_tb")
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account withdrawAccount;
+
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account depositAccount;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    private Long withdrawAccountBalance; // 1111 계좌 -> 1000원 -> 500원 -> 200원
+    private Long depositAccountBalance;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionEnum gubun; // WITHDRAW, DEPOSIT, TRANSFER, ALL
+
+    // 계좌가 사라져도 로그는 남아야 한다.
+    private String sender;
+    private String receiver;
+    private String tel; // ATM -> 1111 입금 (ATM기로 무통장 입금 - 전화번호는 좀 남겨나야 함)
+
+    @CreatedDate // Insert
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // Insert, Update
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Transaction(Long id, Account withdrawAccount, Account depositAccount, Long amount,
+                       Long withdrawAccountBalance, Long depositAccountBalance, TransactionEnum gubun, String sender,
+                       String receiver, String tel, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.withdrawAccount = withdrawAccount;
+        this.depositAccount = depositAccount;
+        this.amount = amount;
+        this.withdrawAccountBalance = withdrawAccountBalance;
+        this.depositAccountBalance = depositAccountBalance;
+        this.gubun = gubun;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.tel = tel;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+
+
+}
