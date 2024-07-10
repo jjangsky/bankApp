fda5e17ffbb84eabd0a56d0bdeea3a4ecbee965d
YouchanKim <dbcks4646@gmail.com>
2024-07-10 23:43:36 +0900

    Security JUnit 인증 테스트

--- bank-app/src/main/java/com/study/bankapp/BankAppApplication.java
+++ bank-app/src/main/java/com/study/bankapp/BankAppApplication.java
@@ -1,15 +1,22 @@
 package com.study.bankapp;
 
+import org.hibernate.validator.internal.engine.ConfigurationImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Arrays;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BankAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankAppApplication.class, args);
+
+		 SpringApplication.run(BankAppApplication.class, args);
+
+
 	}
 
 }
---
+++ bank-app/src/test/java/com/study/bankapp/config/SecurityConfigTest.java
@@ -0,0 +1,47 @@
+package com.study.bankapp.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@AutoConfigureMockMvc  // Mock 환경에서 MockMvc가 등록
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.MOCK)
+class SecurityConfigTest {
+
+    // 가짜 환경에 등록된 MockMvc를 DI함
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("API를 요청하여 인증 테스트를 진행한다.")
+    @Test
+    public void authentication_test() throws Exception{
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/s/test"));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+        // then
+    }
+
+
+
+    @Test
+    public void authorization_test() throws Exception{
+        // given
+
+        // when
+
+        // then
+
+    }
+
+
+
+}
