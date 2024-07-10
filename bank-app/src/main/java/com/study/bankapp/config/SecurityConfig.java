9fb80ce0444eafdd613eb69b0a8e7afa64215a58
YouchanKim <dbcks4646@gmail.com>
2024-07-10 23:24:20 +0900

    boot 3.2 버전으로 설정

--- bank-app/src/main/java/com/study/bankapp/config/SecurityConfig.java
+++ bank-app/src/main/java/com/study/bankapp/config/SecurityConfig.java
@@ -1,2 +1,68 @@
-package com.study.bankapp.config;public class SecurityConfig {
+package com.study.bankapp.config;
+
+import com.study.bankapp.domain.user.UserEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        log.debug("디버그 : BCryptPasswordEncoder 빈 등록");
+        return new BCryptPasswordEncoder();
+    }
+
+    // JWT 필터 등록 필요
+
+
+    // JWT 서버를 만들 예정, Session 사용X
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http.headers(h -> h.frameOptions(f -> f.sameOrigin()));   // iframe 허용 안함
+        http.csrf(cf->cf.disable());      // enable이면 post 맨 작동 안함
+        http.cors(co->co.configurationSource(configurationSource()));
+
+        // JSessionId를 서버쪽에서 관리 안하겠다는 뜻
+        http.sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // react, 앱으로 요청할 예정
+        http.formLogin(f->f.disable());
+        // httpBasic은 브라우저가 팝업창을 이용해서 사용자 인증을 진행한다.
+        http.httpBasic(hb->hb.disable());
+
+        http.authorizeHttpRequests(c->
+                c.requestMatchers("/api/s/**").authenticated()
+                        .requestMatchers("/api/admin/**").hasRole("" + UserEnum.ADMIN)
+                        .anyRequest().permitAll()
+        );
+
+
+        return http.build();
+
+    }
+
+    public CorsConfigurationSource configurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT ,DELETE 허용
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용
+        configuration.setAllowCredentials(true);  // 클라이언트에서 쿠키 요청 허용
+        configuration.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
 }
