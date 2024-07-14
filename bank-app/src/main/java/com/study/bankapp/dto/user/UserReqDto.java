18bd330fd95a0fec761b16a827c4ed5dd6e068a1
YouchanKim <dbcks4646@gmail.com>
2024-07-14 21:53:35 +0900

    정규식 Test 진행

--- bank-app/src/main/java/com/study/bankapp/dto/user/UserReqDto.java
+++ bank-app/src/main/java/com/study/bankapp/dto/user/UserReqDto.java
@@ -3,6 +3,7 @@ package com.study.bankapp.dto.user;
 import com.study.bankapp.domain.user.User;
 import com.study.bankapp.domain.user.UserEnum;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,12 +13,17 @@ public class UserReqDto {
     @Getter
     @Setter
     public static class JoinReqDto{
+        // 영문, 숫자는 되고, 길이 최소 2~20자 이내
+        @Pattern(regexp = "", message = "영문/숫자 2~20자 이내로 작성해 주십시오.")
         @NotEmpty
         private String username;
+        // 길이 4~20
         @NotEmpty
         private String password;
+        // 이메일 형식
         @NotEmpty
         private String email;
+        // 영어, 한글, 1~20
         @NotEmpty
         private String fullname;
 
