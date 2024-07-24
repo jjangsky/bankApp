4d8c4dc03f14e85e299f498c334d96e68571b6d5
jjangsky <dbcks4646@gmail.com>
2024-07-25 00:27:29 +0900

    feat: 계좌 목록 보기 Controller 생성

--- bank-app/src/main/java/com/study/bankapp/web/AccountController.java
+++ bank-app/src/main/java/com/study/bankapp/web/AccountController.java
@@ -2,6 +2,7 @@ package com.study.bankapp.web;
 
 import com.study.bankapp.config.auth.LoginUser;
 import com.study.bankapp.dto.ResponseDto;
+import com.study.bankapp.handler.ex.CustomForbiddenException;
 import com.study.bankapp.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.study.bankapp.dto.account.AccountResponseDto.*;
 import com.study.bankapp.dto.account.AccountRequestDto.*;
 
@@ -30,5 +28,11 @@ public class AccountController {
                                          BindingResult bindingResult){
         AccountSaveRespDto accountSaveRespDto = accountService.creatAccount(accountSaveReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "", null), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser){
+        AccountService.AccountListRespDto accountListRespDto = accountService.getAccountListByUser(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌 목록 보기 유저별 성공", accountListRespDto), HttpStatus.OK);
     }
 }
